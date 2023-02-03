298ef4ce1793adc8c14e9a7a9d2c2970ae605964
Marius Zilinskas <mariuzmail@gmail.com>
2023-02-03 00:32:18 +0000

    1-9 complete

--- src/Outline.java
+++ src/Outline.java
@@ -1,7 +1,5 @@
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 
 public class Outline {
@@ -17,6 +15,7 @@ public class Outline {
     List<String> words = getList();
     System.out.println("1: ");
     // YOUR CODE
+    words.forEach(val -> System.out.println("  " + val));
   }
 
   // Repeat this problem but without two spaces in front of each word.
@@ -26,6 +25,7 @@ public class Outline {
     List<String> words = getList();
     System.out.println("2: ");
     // YOUR CODE
+    words.forEach(System.out::println);
   }
 
   // For each of the following lambda expressions (see Question 5 in Worksheet 2),
@@ -40,6 +40,17 @@ public class Outline {
     List<String> words = getList();
     System.out.println("3:");
     // YOUR CODE
+    List<String> shortWords = words.stream()
+            .filter(s -> s.length() < 4).toList();
+    System.out.println("shortWords: " + shortWords);
+
+    List<String> wordsWithB = words.stream()
+            .filter(s -> s.contains("b")).toList();
+    System.out.println("wordsWithB: " + wordsWithB);
+
+    List<String> evenWords = words.stream()
+            .filter(s -> (s.length() % 2) == 0).toList();
+    System.out.println("evenWords: " + evenWords);
   }
 
 
@@ -55,6 +66,17 @@ public class Outline {
     List<String> words = getList();
     System.out.println("4:");
     // YOUR CODE
+    List<String> excitingWords = words.stream()
+            .map(s -> s + "!").toList();
+    System.out.println("excitingWords: " + excitingWords);
+
+    List<String> eyeWords = words.stream()
+            .map(s -> s.replace("i", "eye")).toList();
+    System.out.println("eyeWords: " + eyeWords);
+
+    List<String> uppercaseWords = words.stream()
+            .map(String::toUpperCase).toList();
+    System.out.println("uppercaseWords: " + uppercaseWords);
   }
 
 
@@ -67,6 +89,21 @@ public class Outline {
     List<String> words = getList();
     System.out.println("5a:");
     // YOUR CODE
+    Optional<String> result = words.stream()
+            .map(String::toUpperCase)
+            .filter(s -> s.length() < 4)
+            .filter(s -> s.contains("E"))
+            .findFirst();
+    System.out.println(result);
+
+    System.out.println("5b:");
+    // YOUR CODE
+    result = words.stream()
+            .map(String::toUpperCase)
+            .filter(s -> s.length() < 4)
+            .filter(s -> s.contains("Q"))
+            .findFirst();
+    result.ifPresent(System.out::println);
   }
 
 
@@ -79,6 +116,14 @@ public class Outline {
     List<String> words = getList();
     System.out.println("6:");
     // YOUR CODE
+    Optional<String> result = words.stream()
+            .map(String::toUpperCase)
+            .peek(s -> System.out.println("pre-filter1: " + s))
+            .filter(s -> s.length() < 4)
+            .peek(s -> System.out.println("pre-filter2: " + s))
+            .filter(s -> s.contains("E"))
+            .findFirst();
+    System.out.println(result);
   }
 
   // (*) Produce a single String that is the result of concatenating the
@@ -91,6 +136,10 @@ public class Outline {
     List<String> words = getList();
     System.out.println("7:");
     // YOUR CODE
+    String concat = words.stream()
+            .map(String::toUpperCase)
+            .reduce("", (a, b) -> a + b);
+    System.out.println(concat);
   }
 
 
@@ -103,6 +152,9 @@ public class Outline {
     List<String> words = getList();
     System.out.println("8:");
     // YOUR CODE
+    String concat = words.stream()
+            .reduce("", (a, b) -> a.toUpperCase() + b.toUpperCase());
+    System.out.println(concat);
   }
 
   // (*) Produce a String that is all the words concatenated together, but
@@ -114,12 +166,140 @@ public class Outline {
     List<String> words = getList();
     System.out.println("9:");
     // YOUR CODE
+//    String concat = words.stream()
+//            .collect(Collectors.joining(","));
+    String concat = String.join(",", words);
+    System.out.println(concat);
   }
 
-  // CONTINUE WITH THE REST OF THE QUESTIONS
+  // ------------------------
+
+  // Use streams to filter the first two meat dishes.
+
+  public static void question10() {
+
+  }
+
+
+  // Count the number of dishes in a stream using the map and reduce methods.
+  //  For the remaining questions, it can be useful to refer to a list of numbers provided,
+  //  for example, by
+  //
+  //```.java
+  //  public static Integer[] getIntegerArray() {
+  //    return new Integer[] { 1, 7, 3, 4, 8, 2 };
+  //  }
+  //```
+
+  public static void question11() {
+
+  }
+
+
+  // Given a list of numbers, print out the list of the squares of each number.
+  // For example, given `[1, 2, 3, 4, 5]` you should print `[1, 4, 9, 16, 25]`.
+
+  public static void question12() {
+
+  }
+
+
+  // Given two lists of numbers, print out all pairs of numbers. For example,
+  // given a list `[1, 2, 3]` and a list `[3, 4]` you should print:
+  //    `[[1, 3], [1, 4], [2, 3], [2, 4], [3, 3], [3, 4]]`.
+  //  For simplicity, you can represent each *pair* as a list with two elements.
+
+  public static void question13() {
+
+  }
+
+
+  // Extend the previous example to return only pairs whose
+  // sum is divisible by `3`. For example, `[2, 4]` and `[3, 3]` are valid.
+
+  public static void question14() {
+
+  }
+
+
+  // (*) Provide three ways to use streams to compute the sum of a list of
+  // numbers.
+
+  public static void question15() {
+
+  }
+
+
+  // (*) Write a static method that produces a `List` of a specified length of
+  //    random numbers. For example,
+  //
+  //```.java
+  //    List<Double> nums = randomNumberList(someSize);
+  //```
+  // Result is something like `[0.7096867136897776, 0.09894202723079482, ...]`
+
+  public static void question16() {
+
+  }
+
+
+  // (*) Write a static method that produces a `List` of numbers that go in order
+  //    by a step size. For example,
+  //
+  //```.java
+  //    List<Integer> nums = orderedNumberList(50, 5, someSize);
+  //```
+  //  Result is `[50, 55, 60, ...]`.
+
+  public static void question17() {
+
+  }
+
+
+  // (*) Rewrite one of the solutions from Question 15 so that it can be executed
+  // in parallel; verify that you get the same answer as for the sequential code.
+
+  public static void question18() {
+
+  }
+
+
+  // (**) Now, use streams to compute the product of some doubles. Show that the *serial*
+  // and *parallel* versions **do not** always result in the same answer.
+  //
+  //    **Note:**
+  //
+  //    This is a bit tricky, because it seems at first that multiplication is associative,
+  //as required by the parallel `reduce`.
+  //    Also, it will be impossible to have differing results if you have a single-core computer!
+
+  public static void question19() {
+
+  }
+
+  // ------------------------
 
   public static void main(String... args) { // varargs alternative to String[]
     question1();
+    question2();
+    question3();
+    question4();
+    question5();
+    question6();
+    question7();
+    question8();
+    question9();
+
+    question10();
+    question11();
+    question12();
+    question13();
+    question14();
+    question15();
+    question16();
+    question17();
+    question18();
+    question19();
 
   }
 }
